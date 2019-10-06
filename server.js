da56c06e2e82ce6c5f2d423458c8585c9e7f44be
KingZak28 <zakariah28@hotmail.com>
2019-10-06 19:14:26 -0400

    Connecting to dialogflow

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const axios = require("axios");
 const bodyParser = require("body-parser");
 const express = require("express");
+const randomize = require("./utils/randomize");
 const app = express();
 const { YELP_KEY } = process.env;
 const yelp = require("yelp-fusion");
@@ -9,16 +9,43 @@ app.use(bodyParser.json());
 
 app.get("/getRestaurants", async (req, res) => {
   try {
+    const location =
+      req.body.queryResult &&
+      req.body.queryResult.parameters &&
+      req.body.queryResult.parameters.geocity
+        ? req.body.queryResult.parameters.geocity
+        : "Liverpool england"; //Default city for now
     const yelpResponse = await client.search({
-      location: "waterloo"
+      location: location
     });
 
     const data = yelpResponse.jsonBody.businesses;
+    console.log(data);
 
-    const names = data.map(entry => `${entry.name} rated at ${entry.rating}`);
-    console.log(names);
+    const names = data.map(
+      entry =>
+        `${entry.name} at ${entry.location.address1} rated at ${entry.rating} stars`
+    );
+    const message = `My friends at yelp say that ${randomize(
+      names
+    )} in ${location} is quite the restaurant indeed. My contacts never let me down, a butler is well connected you know!`;
+    const response = {
+      google: {
+        expectUserResponse: true,
+        richResponse: {
+          items: [
+            {
+              simpleResponse: {
+                textToSpeech: message
+              }
+            }
+          ]
+        }
+      }
+    };
+    console.log(message);
 
-    res.json({ code: "Success" });
+    res.json({ payload: response });
   } catch (err) {
     console.log("Error Encountered: " + err);
     res.json({
---
+++ utils/randomize.js
@@ -0,0 +1,5 @@
+module.exports = arr => {
+  let i = 0;
+  i = Math.floor(Math.random() * arr.length);
+  return arr[i];
+};
