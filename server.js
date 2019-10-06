4f89c135a83687b03d4b733b7b88385b4dc218ef
KingZak28 <zakariah28@hotmail.com>
2019-10-06 18:11:59 -0400

    test yelp response working

---
+++ server.js
@@ -0,0 +1,29 @@
+const axios = require("axios");
+const bodyParser = require("body-parser");
+const express = require("express");
+const app = express();
+const { YELP_KEY } = process.env;
+const yelp = require("yelp-fusion");
+const client = yelp.client(YELP_KEY);
+app.use(bodyParser.json());
+
+app.get("/getRestaurants", async (req, res) => {
+  try {
+    const yelpResponse = await client.search({
+      location: "waterloo"
+    });
+
+    const data = yelpResponse.jsonBody.businesses;
+
+    const names = data.map(entry => `${entry.name} rated at ${entry.rating}`);
+    console.log(names);
+
+    res.json({ code: "Success" });
+  } catch (err) {
+    console.log("Error Encountered: " + err);
+  }
+});
+
+app.listen(process.env.PORT || 8000, () => {
+  console.log("Server is alive G");
+});
