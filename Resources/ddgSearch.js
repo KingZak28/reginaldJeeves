c062bcf09a957d0b4244832af0f32f6a174ee469
KingZak28 <zakariah28@hotmail.com>
2019-10-12 15:21:01 -0400

    Returning query result

--- Resources/ddgSearch.js
+++ Resources/ddgSearch.js
@@ -4,20 +4,25 @@ const { DDG } = require("node-ddg-api");
 const ddg = new DDG("reginaldJeeves");
 
 const ddgSearch = query => {
-  ddg.instantAnswer(query, { skip_disambig: "0" }, (err, response) => {
-    let message;
-    try {
-      const abstract = response.Abstract;
-      const text = response.RelatedTopics[0].Text;
-      abstract.length > text.length
-        ? (message = `My friends at duck duck go tell me that: ${abstract}`)
-        : (message = `My associates at duck duck go tell me that: ${text}`);
-    } catch (err) {
-      console.log(`Error encountered: ${err}`);
-      message =
-        "My apologies friend, I'm having trouble contacting my associates";
+  const response = ddg.instantAnswer(
+    query,
+    { skip_disambig: "0" },
+    (err, response) => {
+      let message;
+      try {
+        const abstract = response.Abstract;
+        const text = response.RelatedTopics[0].Text;
+        abstract.length > text.length
+          ? (message = `My friends at duck duck go tell me that: ${abstract}`)
+          : (message = `My associates at duck duck go tell me that: ${text}`);
+      } catch (err) {
+        console.log(`Error encountered: ${err}`);
+        message =
+          "My apologies friend, I'm having trouble contacting my associates";
+      }
+      return message;
     }
-    return message;
-  });
+  );
+  return response;
 };
 module.exports = ddgSearch;
