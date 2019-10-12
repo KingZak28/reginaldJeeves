651834a79688ca1e481d8c478db3e8f38664e7d7
KingZak28 <zakariah28@hotmail.com>
2019-10-12 17:05:27 -0400

    DDG wrapper change

--- Resources/ddgSearch.js
+++ Resources/ddgSearch.js
@@ -1,25 +1,24 @@
 const randomize = require("../utils/randomize");
 const language = require("../Language/en.json");
-const { DDG } = require("node-ddg-api");
-const ddg = new DDG("reginaldJeeves");
+const { duckIt } = require("node-duckduckgo");
 
-const ddgSearch = query => {
-  ddg.instantAnswer(query, { skip_disambig: "0" }, (err, response) => {
-    try {
-      const abstract = response.Abstract;
-      const text = response.RelatedTopics[0].Text;
-      abstract.length > text.length
-        ? (message = `My friends at duck duck go tell me that: ${abstract}`)
-        : (message = `My associates at duck duck go tell me that: ${text}`);
-    } catch (err) {
-      console.log(`Error encountered: ${err}`);
-      message =
-        "My apologies friend, I'm having trouble contacting my associates";
-    }
-
-    console.log(`Message here is: ${message}`);
+const ddgSearch = async query => {
+  try {
+    const result = await duckIt("netflix");
+    console.log(result.data.AbstractText);
+    const abstract = result.data.AbstractText;
+    const text = result.data.RelatedTopics[0].Text;
+    abstract.length > text.length
+      ? (message = `My friends at duck duck go tell me that: ${abstract}`)
+      : (message = `My associates at duck duck go tell me that: ${text}`);
+    console.log(message);
     return message;
-  });
-  console.log(`ddg: ${ddg}`);
+  } catch (err) {
+    console.error("Error Encountered: ", err);
+    message =
+      "Apologies friend, I'm having trouble searching for that right now";
+    return message;
+  }
 };
+
 module.exports = ddgSearch;
