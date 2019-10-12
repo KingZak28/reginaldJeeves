1aa1ebb62179bbfd356a0ac979c36de9b031246f
KingZak28 <zakariah28@hotmail.com>
2019-10-12 10:12:52 -0400

    Begining to modularize

---
+++ Resources/yelpMessage.js
@@ -0,0 +1,39 @@
+const { YELP_KEY } = process.env;
+const yelp = require("yelp-fusion");
+const client = yelp.client(YELP_KEY);
+const language = require("./en.json.js");
+
+const yelpMessage = async req => {
+  let message = "test";
+  try {
+    const location =
+      req.body.queryResult &&
+      req.body.queryResult.parameters &&
+      req.body.queryResult.parameters.geocity
+        ? req.body.queryResult.parameters.geocity
+        : "Liverpool england"; //Default city for now
+    const yelpResponse = await client.search({
+      location: location
+    });
+
+    console.log(yelpResponse.jsonBody);
+    const data = yelpResponse.jsonBody.businesses;
+
+    const names = data.map(
+      entry =>
+        `${entry.name} at ${entry.location.address1} rated at ${entry.rating} stars`
+    );
+    message = `${randomize(language.restaurantPrefixes)} ${randomize(
+      names
+    )} in ${location} ${randomize(language.responseRandomizer)} ${randomize(
+      language.responseSuffixes
+    )}`;
+  } catch (err) {
+    console.log(`Encountered this error: ${err}`);
+    message =
+      "I'm having trouble getting ahold of my contacts at this moment in time please try again later.";
+  }
+  return message;
+};
+
+module.exports = yelpMessage;
--- server.js
+++ server.js
@@ -1,49 +1,14 @@
 const bodyParser = require("body-parser");
 const express = require("express");
 const randomize = require("./utils/randomize");
-const { WebhookClient, Card } = require("dialogflow-fulfillment");
+const { WebhookClient } = require("dialogflow-fulfillment");
 const app = express();
-const { YELP_KEY } = process.env;
-const yelp = require("yelp-fusion");
-const client = yelp.client(YELP_KEY);
-const language = require("./en.json");
+const yelpMessage = require("./Resources/yelpMessage");
 app.use(bodyParser.json());
-
-const yelpMessage = async req => {
-  let message = "test";
-  try {
-    const location =
-      req.body.queryResult &&
-      req.body.queryResult.parameters &&
-      req.body.queryResult.parameters.geocity
-        ? req.body.queryResult.parameters.geocity
-        : "Liverpool england"; //Default city for now
-    const yelpResponse = await client.search({
-      location: location
-    });
-
-    console.log(yelpResponse.jsonBody);
-    const data = yelpResponse.jsonBody.businesses;
-
-    const names = data.map(
-      entry =>
-        `${entry.name} at ${entry.location.address1} rated at ${entry.rating} stars`
-    );
-    message = `${randomize(language.restaurantPrefixes)} ${randomize(
-      names
-    )} in ${location} is quite the restaurant indeed. ${randomize(
-      language.responseSuffixes
-    )}`;
-  } catch (err) {
-    console.log(`Encountered this error: ${err}`);
-    message =
-      "I'm having trouble getting ahold of my contacts at this moment in time please try again later.";
-  }
-  return message;
-};
 
 const webhookProcessing = async (req, res, msg) => {
   const agent = new WebhookClient({ request: req, response: res });
+  console.log(agent.parameters);
   const message = await msg;
   const restaurantIntent = agent => {
     console.log(`Here inside restaurant intent: ${message}`);
