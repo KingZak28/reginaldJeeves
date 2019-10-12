a2288b099ce0af1b7cb9f58cb77235c3c1180b30
KingZak28 <zakariah28@hotmail.com>
2019-10-12 16:04:11 -0400

    Debugging

--- Resources/ddgSearch.js
+++ Resources/ddgSearch.js
@@ -3,21 +3,25 @@ const language = require("../Language/en.json");
 const { DDG } = require("node-ddg-api");
 const ddg = new DDG("reginaldJeeves");
 
-ddg.instantAnswer((query = ""), { skip_disambig: "0" }, (err, response) => {
-  try {
-    const abstract = response.Abstract;
-    const text = response.RelatedTopics[0].Text;
-    abstract.length > text.length
-      ? (message = `My friends at duck duck go tell me that: ${abstract}`)
-      : (message = `My associates at duck duck go tell me that: ${text}`);
-  } catch (err) {
-    console.log(`Error encountered: ${err}`);
-    message =
-      "My apologies friend, I'm having trouble contacting my associates";
-  }
+const ddgSearch = query => {
+  let arr = [];
+  ddg.instantAnswer(query, { skip_disambig: "0" }, (err, response) => {
+    try {
+      const abstract = response.Abstract;
+      const text = response.RelatedTopics[0].Text;
+      abstract.length > text.length
+        ? (message = `My friends at duck duck go tell me that: ${abstract}`)
+        : (message = `My associates at duck duck go tell me that: ${text}`);
+    } catch (err) {
+      console.log(`Error encountered: ${err}`);
+      message =
+        "My apologies friend, I'm having trouble contacting my associates";
+    }
 
-  console.log(`Message here is: ${message}`);
-  return message;
-});
-
+    console.log(`Message here is: ${message}`);
+    arr.push(message);
+    return message;
+  });
+  console.log(`arr is: ${arr}`);
+};
 module.exports = ddg.instantAnswer;
